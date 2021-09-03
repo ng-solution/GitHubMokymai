6d58512cf3ebb611a00092c00aff5e9e7b053d28
ng-solution <89936592+ng-solution@users.noreply.github.com>
2021-09-03 19:39:55 +0300

    Mano pirmas bandymas

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Sarasas = new List<string>();
+            foreach (string itm in sarasas)
+            {
+                Console.WriteLine(itm);
+            }
+        }
+    }
+}
